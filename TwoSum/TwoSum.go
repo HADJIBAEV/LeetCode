2e682b4dede56eddf6e38ea2e1a5190a31499e15
Kahramon <hadjibaevkahramon@gmail.com>
2023-09-01 11:32:38 -0700

    Add second solution

--- TwoSum/TwoSum.go
+++ TwoSum/TwoSum.go
@@ -13,8 +13,23 @@ func main() {
 	/*nums := []int{3, 3}
 	target := 6 */
 	fmt.Println(TwoSum(nums, target))
+	fmt.Println(TwoSum2(nums, target))
 }
+
+// TwoSum width loop
 func TwoSum(nums []int, target int) []int {
+	for i, left := range nums {
+		for j, right := range nums {
+			if left+right == target && i != j {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+// TwoSum2 width map
+func TwoSum2(nums []int, target int) []int {
 	m := make(map[int]int)     //a+b = target
 	for i, num := range nums { //a = nums[i], target   //b = target - a
 		if idx, ok := m[target-num]; ok {
